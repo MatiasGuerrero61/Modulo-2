c6e7c7635469b38fe4d421ee299f5106e8f04fbc
MatiasGuerrero61 <guerreromatias24@gmail.com>
2020-01-27 17:23:55 -0300

    select completo

--- task 2/js/senate.js
+++ task 2/js/senate.js
@@ -2,11 +2,15 @@ let senadores=sdata.results[0].members;
 
 function notNull(valor){if(valor == null){return ""} return valor}
 
-function filtrar(){
+function filtrarPorPartido(){
     document.getElementById("senate-data").innerHTML = "";
+    document.getElementById("select-senator").innerHTML = `<option value="all"> All </option>`
     let check = document.getElementsByClassName("congress");
+    let senadoresFiltrados = ""
+    let select=[]
     for(let i=0;check.length; i++){
         if(check[i].checked){
+            senadoresFiltrados = senadores.filter(e => e.party == check[i].value)
             document.getElementById("senate-data").innerHTML+= senadores.filter(e => e.party == check[i].value).map((e)=>
             `<tr> <td> ${e.first_name} ${notNull(e.middle_name)} ${e.last_name} </td>
             <td> ${e.party} </td>
@@ -14,11 +18,21 @@ function filtrar(){
             <td> ${e.seniority} </td>
             <td> ${e.votes_with_party_pct}% </td> </tr>`
             ).join("");
+
+            senadoresFiltrados.forEach(function(e){
+                if(select.indexOf(e.state) == (-1)){
+                    select.push(e.state)
+                }
+            });
+            document.getElementById("select-senator").innerHTML += select.map(e => 
+                `<option value="${e}" class="state">${e}</option>`).join("")
+        }
         }
     }
-}
-document.getElementById("rep").addEventListener("click", filtrar)
-document.getElementById("dem").addEventListener("click", filtrar)
-document.getElementById("ind").addEventListener("click", filtrar)
+document.getElementById("rep").addEventListener("click", filtrarPorPartido)
+document.getElementById("dem").addEventListener("click", filtrarPorPartido)
+document.getElementById("ind").addEventListener("click", filtrarPorPartido)
+
+
 
 
