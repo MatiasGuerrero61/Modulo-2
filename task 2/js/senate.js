07bd4857b5a4b85f4b71d9272cf3d53958a8fd1e
MatiasGuerrero61 <guerreromatias24@gmail.com>
2020-01-29 14:20:44 -0300

    filtro por estado ok

--- task 2/js/senate.js
+++ task 2/js/senate.js
@@ -2,16 +2,16 @@ let senadores=sdata.results[0].members;
 
 function notNull(valor){if(valor == null){return ""} return valor}
 
-function filtrarPorPartido(){
+function filtrar(){
     document.getElementById("senate-data").innerHTML = "";
-    document.getElementById("select-senator").innerHTML = `<option value="all"> All </option>`
     let check = document.getElementsByClassName("congress");
     let senadoresFiltrados = ""
     let select=[]
+    let state = document.getElementById("select-senator").value
     for(let i=0;check.length; i++){
         if(check[i].checked){
             senadoresFiltrados = senadores.filter(e => e.party == check[i].value)
-            document.getElementById("senate-data").innerHTML+= senadores.filter(e => e.party == check[i].value).map((e)=>
+            document.getElementById("senate-data").innerHTML+= senadores.filter(e => e.party == check[i].value && (e.state == state || state == "all")).map((e)=>
             `<tr> <td> <a href="${e.url}">${e.first_name} ${notNull(e.middle_name)} ${e.last_name} </a></td>
             <td> ${e.party} </td>
             <td> ${e.state} </td>
@@ -20,19 +20,26 @@ function filtrarPorPartido(){
             ).join("");
             
             senadoresFiltrados.forEach(function(e){
+                
                 if(select.indexOf(e.state) == (-1)){
                     select.push(e.state)
+                    
                 }
+                document.getElementById("select-senator").innerHTML = `<option value ="all">All</option>`
+                document.getElementById("select-senator").innerHTML += select.map(e => 
+                    `<option value="${e}" class="state">${e}</option>`).join("")
             });
-            document.getElementById("select-senator").innerHTML += select.map(e => 
-                `<option value="${e}" class="state">${e}</option>`).join("")
+            
+
+                document.getElementById("select-senator").value = state
         }
         }
     }
-document.getElementById("rep").addEventListener("click", filtrarPorPartido)
-document.getElementById("dem").addEventListener("click", filtrarPorPartido)
-document.getElementById("ind").addEventListener("click", filtrarPorPartido)
+document.getElementById("rep").addEventListener("click", filtrar)
+document.getElementById("dem").addEventListener("click", filtrar)
+document.getElementById("ind").addEventListener("click", filtrar)
+document.getElementById("select-senator").addEventListener("change", filtrar)
 
-filtrarPorPartido()
+filtrar()
 
 
