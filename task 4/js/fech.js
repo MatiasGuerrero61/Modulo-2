6e7f6515ea5d1bd59d286b9c11f5e3c5a870cd6c
MatiasGuerrero61 <guerreromatias24@gmail.com>
2020-02-18 17:53:37 -0300

    task 4 terminada

--- task 4/js/fech.js
+++ task 4/js/fech.js
@@ -13,7 +13,29 @@ const app = new Vue({
         states:[],
         stateActual: "All",
         campos:["party", "votes_with_party_pct", "state", "seniority"],
-        current:'home'
+        current:'home',
+        stats:{
+            republican:{
+            cant:0,
+            votes:0
+            },
+            democrat:{
+            cant:0,
+            votes:0
+            },
+           independent:{
+            cant:0,
+            votes:0,
+           },
+           total:{
+            cant:0
+           }            
+        },
+        tenPercent:0,
+        leastEngaged:[],
+        mostEngaged:[],
+        leastLoyal:[],
+        mostLoyal:[]
     },
     created(){
         fetch(this.url, this.init).then(function(res){
@@ -27,6 +49,11 @@ const app = new Vue({
           app.members = json.results[0].members
           app.parties = app.getKeyValue(json.results[0].members, "party")
           app.states = app.getKeyValue(json.results[0].members, "state")
+          app.partyFilter(app.members)
+          app.leastEngaged = app.getArray(app.members, false,"missed_votes_pct")
+          app.mostEngaged = app.getArray(app.members, true, "missed_votes_pct")
+          app.mostLoyal = app.getArray(app.members, true, "votes_with_party_pct")
+          app.leastLoyal = app.getArray(app.members, false, "votes_with_party_pct")
  
         })
         .catch(function(error){
@@ -45,6 +72,47 @@ const app = new Vue({
             }else{
                 return string[0].toUpperCase() + string.slice(1)
             }
+        },
+        partyFilter(array){
+            array.forEach(e =>{
+                if(e.party == "R"){
+                    app.stats.republican.cant ++
+                    app.stats.republican.votes += e.votes_with_party_pct
+                }
+                if(e.party == "D"){
+                    app.stats.democrat.cant ++
+                    app.stats.democrat.votes += e.votes_with_party_pct
+                }
+                if(e.party == "I"){
+                    app.stats.independent.cant ++
+                    app.stats.independent.votes += e.votes_with_party_pct
+                }
+            })
+            app.stats.democrat.votes = app.stats.democrat.cant ? app.stats.democrat.votes / app.stats.democrat.cant: 0           
+            app.stats.republican.votes = app.stats.republican.cant ? app.stats.republican.votes / app.stats.republican.cant: 0
+            app.stats.independent.votes = app.stats.independent.cant ? app.stats.independent.votes / app.stats.independent.cant: 0
+            app.stats.total.cant = array.length
+        },
+        getArray(array, orden, filtro){
+            let aux=[]
+            let result=[]
+            let i =0
+            this.tenPercent = Math.round(array.length * 0.10)
+            aux = array.filter(e=> e.total_votes != 0)
+            if(orden){
+                aux.sort(function(a,b){
+                    return a[filtro]-b[filtro]
+                })
+            }else{
+                aux.sort(function(a,b){
+                    return b[filtro]-a[filtro]
+                }) 
+            }
+            while(i < this.tenPercent || aux[i] == aux[i-1]){
+                result.push(aux[i])
+                i ++
+            }
+            return result   
         }
     },
     computed:{
